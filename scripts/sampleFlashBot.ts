5021caa3710c99604a51fca17bd397e51a98df70
Luke-Longo <lukelongo0421@gmail.com>
2022-10-27 15:34:37 -0400

    added comment on data field

--- scripts/sampleFlashBot.ts
+++ scripts/sampleFlashBot.ts
@@ -18,6 +18,8 @@ let wallet = new Wallet(PRIVATE_KEY!)
 
 const nonce = await alchemy.core.getTransactionCount(wallet.address, "latest")
 
+// you can use the data field to call specific functions on a contract, ie call the initiate swap on the paiFlash contract
+
 let exampleTx = {
     to: "0x4b9007B0BcE78cfB634032ec31Ed56adB464287b",
     value: 10,
@@ -26,6 +28,7 @@ let exampleTx = {
     nonce: nonce,
     type: 2,
     chainId: 5,
+    data: "0x",
 }
 
 let rawTransaction = await wallet.signTransaction(exampleTx)
